7b8cbed3b2c2f45c3483282a6d62ee46b9fa2592
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2023-11-13 13:00:50 +0100

    meiHead parsing

--- scripts/music-representation2music-meta/2.make-ttl.py
+++ scripts/music-representation2music-meta/2.make-ttl.py
@@ -53,6 +53,7 @@ def normalise(s):
 
 def extractComposers(file, data):
     composers = []
+    attributedComposers = []
 
     # fileDesc > titleStmt > respStmt > persName{@role=composer}
     with suppress(KeyError):
@@ -77,7 +78,7 @@ def extractComposers(file, data):
     # extMeta > frames > metaFrame > frameInfo > referenceValue when referenceKey=COM
     with suppress(KeyError):
         for metaFrame in data['meiHead']['extMeta']['frames']:
-            if metaFrame['frameInfo']['referenceKey'] in ['COM', 'COM2']:
+            if metaFrame['frameInfo']['referenceKey'] in ['COM', 'COM1', 'COM2']:
                 composers.append(metaFrame['frameInfo']['referenceValue'])
 
     # fileDesc > sourceDesc > source > titleStmt > respStmt > name{@role=composer}
@@ -112,18 +113,46 @@ def extractComposers(file, data):
             if data['meiHead']['workDesc']['work']['titleStmt']['respStmt']['name']['role'] == 'composer':
                 composers.append(data['meiHead']['workDesc']['work']['titleStmt']['respStmt']['name']['value'])
 
+    # Détection du compositeur attribué
+
+    # extMeta > frames > metaFrame > frameInfo > referenceValue when referenceKey=COA
+    with suppress(KeyError):
+        for metaFrame in data['meiHead']['extMeta']['frames']:
+            if metaFrame['frameInfo']['referenceKey'] in ['COA', 'COA1', 'COA2']:
+                if metaFrame['frameInfo']['referenceValue'] in composers:
+                    composers.remove(metaFrame['frameInfo']['referenceValue'])
+                attributedComposers.append(metaFrame['frameInfo']['referenceValue'])
+
     if len(composers) == 0:
         print(file)
 
-    return list(set([clean(c) for c in composers]))
+    composers = list(set([clean(c) for c in composers]))
+    attributedComposers = list(set([clean(c) for c in attributedComposers]))
+
+    return {
+        "composers": composers,
+        "attributedComposers": attributedComposers
+    }
+
+
+def extractTitles(file, data):
+    titles = []
+
+    # fileDesc > titleStmt > title
+    with suppress(KeyError):
+        titles.append(data['meiHead']['fileDesc']['titleStmt']['title'])
+
+    return titles
 
 
 # CALL SHERLOCK API
 
 # mei_files_url = ['https://raw.githubusercontent.com/polifonia-project/tonalities_pilot/main/scores/De_Rore/De_Rore_Di_tempo_in_tempo_mi_si_fa_men_dura.mei']
 
-for x in mei_files_url:
+for i, x in enumerate(mei_files_url):
     x = x.replace('\n', '')
+    print(
+        f"🌲 [{i}/{len(mei_files_url)}] {x.replace('https://raw.githubusercontent.com/polifonia-project/tonalities_pilot/main/scores', '')}")
     r = requests.post(
         conf['sherlock']['service']['mei']['head'],
         data=json.dumps({'file_url': x}),
@@ -132,13 +161,17 @@ for x in mei_files_url:
     try:
         r.raise_for_status()
         data = r.json()
-        print(x)
-        # pprint(data)
+
         composers = extractComposers(x, data)
-        print(composers)
+        titles = extractTitles(x, data)
+
+        print(f"{' ' * 11}TITLE:                {'•'.join(titles)}")
+        print(f"{' ' * 11}COMPOSERS:            {'•'.join(composers['composers'])}")
+        print(f"{' ' * 11}ATTRIBUTES COMPOSERS: {'•'.join(composers['attributedComposers'])}")
+
     except requests.exceptions.HTTPError as err:
         pass
-        # pprint(x)
-        # pprint('ERROR')
-        # pprint(r)
-        # pprint(err)
+        pprint(x)
+        pprint('ERROR')
+        pprint(r)
+        pprint(err)
