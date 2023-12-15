6d99e2b9c1d739a4942a3f7c8b2f9393ef8737c7
Thomas ⬡ Bottini <thomas.bottini@gmail.com>
2023-12-15 11:48:04 +0100

    MEI2MM

---
+++ scripts/meihead-parser/3.make-ttl-music-meta.py
@@ -0,0 +1,50 @@
+import glob
+import json
+import os
+from rdflib import DCTERMS, Graph, Namespace, OWL, RDF, RDFS
+import sys
+
+################################################################################
+# SETUP
+################################################################################
+
+g = Graph()
+
+PC = Namespace('https://w3id.org/polifonia/ontology/core/')
+MR = Namespace('https://w3id.org/polifonia/ontology/music-representation/')
+MM = Namespace('https://w3id.org/polifonia/ontology/music-meta/')
+
+g.bind('dcterms', DCTERMS)
+g.bind('owl', OWL)
+g.bind('rdf', RDF)
+g.bind('rdfs', RDFS)
+g.bind('core', PC)
+g.bind('mr', MR)
+g.bind('mm', MM)
+
+metadata_json_files = glob.glob('./metadata-json/**/*.json', recursive=True)
+
+sys.path.append(os.path.abspath(os.path.join('./sherlockcachemanagement', '')))
+from sherlockcachemanagement import Cache  # nopep8
+cache = Cache('cache.yaml')
+
+################################################################################
+# PROCESS DATA…
+################################################################################
+
+for f in metadata_json_files:
+    f = open(f)
+    data = json.load(f)
+    f.close()
+
+    for k, v in data.items():
+        if not v:
+            continue
+        print(v)
+
+################################################################################
+# THAT'S ALL FOLKS!
+################################################################################
+
+g.serialize(destination="tonalities-mei-corpus-musicmeta.ttl")
+cache.bye()
